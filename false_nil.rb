261b15caaa01d8849c5fc3c27a84b64daaad6510
Ricardo Baumann <ricardo.baumann@zenvia.com.br>
2014-09-08 16:56:39 -0300

    pushing examples

---
+++ false_nil.rb
@@ -0,0 +1,10 @@
+puts NIL.class # NilClass  
+puts nil.class # NilClass  
+puts nil.object_id # 4  
+puts NIL.object_id # 4  
+  
+# FALSE is synonym for false  
+puts FALSE.class # FalseClass  
+puts false.class # FalseClass  
+puts false.object_id # 0  
+puts FALSE.object_id # 0 
---
+++ statement_modifier.rb
@@ -0,0 +1,2 @@
+puts "Assim é melhor" if 1 > 0
+puts "Assim também" unless 1 == 0
